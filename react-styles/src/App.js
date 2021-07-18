2a547ca8bf8d7799abe801c19ce20be45361cd37
saadcsku <saadcsku@gmail.com>
2021-07-18 08:39:33 -0700

    css style added

--- react-styles/src/App.js
+++ react-styles/src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css"
 
 function App() {
+
+  const css={
+      color:'white',
+      fontSize:"50px",
+      backgroundColor: "red"
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1 style={css}>This css is coming from page css</h1>
+       <h2 className="h2style">This css is coming from external css file</h2>
       </header>
     </div>
   );
