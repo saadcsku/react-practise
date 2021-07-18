4ab79b1426aa9da8439146d49fcb1dfce1782b1b
saadcsku <saadcsku@gmail.com>
2021-07-18 11:28:56 -0700

    state updated

--- react-state/src/App.js
+++ react-state/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  let a = 0;
+  let [val, valUpdator]=useState(a);
+
+  const Inc = () =>{
+    val=val+1
+    valUpdator(val);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter {val}</h1>
+      <button onClick={Inc}>Add</button>
     </div>
   );
 }
