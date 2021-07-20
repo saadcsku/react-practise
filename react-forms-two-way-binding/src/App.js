77dc17bed117438508df522951e6ea544cff02d1
saadcsku <saadcsku@gmail.com>
2021-07-20 04:16:09 -0700

    two way binding added

--- react-forms-two-way-binding/src/App.js
+++ react-forms-two-way-binding/src/App.js
@@ -1,14 +1,48 @@
+import React,{useState} from 'react'
 import './App.css';
 
 function App() {
+
+     const [name , SetName]=useState('');
+     const [pickup , SetPickup]=useState('');
+     const [instruction , SetInstruction]=useState('');
+
+   const SubmitHandler=(e) =>{
+      e.preventDefault();
+      const submitData={
+         submitName: name,
+         submitPickup: pickup,
+         submitInstructions: instruction
+      }
+
+      console.log(submitData);
+      SetName('');
+      SetPickup('');
+      SetInstruction('');
+   }
+
+   const NameHandler=(e) =>{
+      SetName(e.target.value);
+      
+   }
+   
+   const PickupHandler=(e) =>{
+      SetPickup(e.target.value);
+   }
+   
+   const InstructionHandler=(e) =>{
+      SetInstruction(e.target.value);
+   }
+
+
   return (
 
-   <form className="myForm">
-      <label>Name <input type="text" name="customer_name"  required/> </label> 
+   <form className="myForm" onSubmit={SubmitHandler}>
+      <label>Name <input type="text" value={name} name="customer_name"  required onChange={NameHandler}/> </label> 
       <p>
          <label>
             Pickup Place
-            <select id="pickup_place" name="pickup_place">
+            <select id="pickup_place" value={pickup} name="pickup_place" onChange={PickupHandler}>
                <option value="" >Select One</option>
                <option value="office" >Taxi Office</option>
                <option value="town_hall" >Town Hall</option>
@@ -18,10 +52,10 @@ function App() {
       </p>
       <p>
          <label>Special Instructions
-         <textarea name="comments"></textarea>
+         <textarea name="comments" value={instruction} onChange={InstructionHandler} />
          </label>
       </p>
-      <p><button>Submit Booking</button></p>
+      <p><button>Submit</button></p>
    </form>
 
   );
